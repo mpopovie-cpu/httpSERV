49e8a893cc0a60d5203e69dd01de40de1020b1e3
Volodymir Popov <m.popov.ie@gmail.com>
2026-04-10 10:35:20 +0100

    Add HttpResponse class with status codes and JSON body support

---
+++ src/main/java/ru/otus/java/basic/april/server/processors/CreatePageRequestProcessor.java
@@ -0,0 +1,15 @@
+package ru.otus.java.basic.april.server.processors;
+
+import ru.otus.java.basic.april.server.HttpRequest;
+import ru.otus.java.basic.april.server.HttpResponse;
+import ru.otus.java.basic.april.server.TemplateLoader;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class CreatePageRequestProcessor implements RequestProcessor {
+    @Override
+    public void execute(HttpRequest request, OutputStream output) throws IOException {
+        HttpResponse.ok(TemplateLoader.load("templates/create.html")).send(output);
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/april/server/processors/GetItemsApiRequestProcessor.java
@@ -0,0 +1,21 @@
+package ru.otus.java.basic.april.server.processors;
+
+import ru.otus.java.basic.april.server.HttpRequest;
+import ru.otus.java.basic.april.server.HttpResponse;
+import ru.otus.java.basic.april.server.ItemRepository;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GetItemsApiRequestProcessor implements RequestProcessor {
+    private final ItemRepository repository;
+
+    public GetItemsApiRequestProcessor(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void execute(HttpRequest request, OutputStream output) throws IOException {
+        HttpResponse.okJson(repository.findAll()).send(output);
+    }
+}
